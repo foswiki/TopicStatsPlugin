4538506a73190120faea1ec08a4b815909cb8090
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/TopicStatsPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/TopicStatsPlugin.pm
+++ lib/Foswiki/Plugins/TopicStatsPlugin.pm
@@ -69,118 +69,118 @@ FOOBARSOMETHING. This avoids namespace issues.
 =cut
 
 sub initPlugin {
-	my ( $topic, $web, $user, $installWeb ) = @_;
+    my ( $topic, $web, $user, $installWeb ) = @_;
 
-	# check for Plugins.pm versions
-	if ( $Foswiki::Plugins::VERSION < 2.0 ) {
-		Foswiki::Func::writeWarning( 'Version mismatch between ',
-			__PACKAGE__, ' and Plugins.pm' );
-		return 0;
-	}
+    # check for Plugins.pm versions
+    if ( $Foswiki::Plugins::VERSION < 2.0 ) {
+        Foswiki::Func::writeWarning( 'Version mismatch between ',
+            __PACKAGE__, ' and Plugins.pm' );
+        return 0;
+    }
 
-	# Register the _EXAMPLETAG function to handle %EXAMPLETAG{...}%
-	# This will be called whenever %EXAMPLETAG% or %EXAMPLETAG{...}% is
-	# seen in the topic text.
-	Foswiki::Func::registerTagHandler( 'TOPICSTATS', \&_TOPICSTATS );
+    # Register the _EXAMPLETAG function to handle %EXAMPLETAG{...}%
+    # This will be called whenever %EXAMPLETAG% or %EXAMPLETAG{...}% is
+    # seen in the topic text.
+    Foswiki::Func::registerTagHandler( 'TOPICSTATS', \&_TOPICSTATS );
 
-	# Plugin correctly initialized
-	return 1;
+    # Plugin correctly initialized
+    return 1;
 }
 
 # The function used to handle the %TOPICSTATS{...}% macro
 # You would have one of these for each macro you want to process.
 sub _TOPICSTATS {
-	my ( $session, $params, $topic, $web ) = @_;
+    my ( $session, $params, $topic, $web ) = @_;
 
-	# $session  - a reference to the Foswiki session object (if you don't know
-	#             what this is, just ignore it)
-	# $params=  - a reference to a Foswiki::Attrs object containing
-	#             parameters.
-	#             This can be used as a simple hash that maps parameter names
-	#             to values, with _DEFAULT being the name for the default
-	#             (unnamed) parameter.
-	# $topic - name of the topic in the query
-	# $web   - name of the web in the query
-	# Return: the result of processing the macro. This will replace the
-	# macro call in the final text.
+    # $session  - a reference to the Foswiki session object (if you don't know
+    #             what this is, just ignore it)
+    # $params=  - a reference to a Foswiki::Attrs object containing
+    #             parameters.
+    #             This can be used as a simple hash that maps parameter names
+    #             to values, with _DEFAULT being the name for the default
+    #             (unnamed) parameter.
+    # $topic - name of the topic in the query
+    # $web   - name of the web in the query
+    # Return: the result of processing the macro. This will replace the
+    # macro call in the final text.
 
-	my $logDate  = $params->{logdate} || '';
-	my $script   = $params->{script}  || 'all';
-	my $maxItems = $params->{max}     || 25;
-	$topic = $params->{topic} if $params->{topic};
-	$web   = $params->{web}   if $params->{web};
+    my $logDate  = $params->{logdate} || '';
+    my $script   = $params->{script}  || 'all';
+    my $maxItems = $params->{max}     || 25;
+    $topic = $params->{topic} if $params->{topic};
+    $web   = $params->{web}   if $params->{web};
 
-	$maxItems = 25    if $maxItems !~ /^\d+/;
-	$script   = 'all' if $script !~ /view|save|all/;
+    $maxItems = 25    if $maxItems !~ /^\d+/;
+    $script   = 'all' if $script !~ /view|save|all/;
 
-	require Foswiki::Time;
-	unless ($logDate) {
-		$logDate =
-		  Foswiki::Time::formatTime( time(), '$year$mo', 'servertime' );
-	}
+    require Foswiki::Time;
+    unless ($logDate) {
+        $logDate =
+          Foswiki::Time::formatTime( time(), '$year$mo', 'servertime' );
+    }
 
-	my $logMonth;
-	my $logYear;
-	if ( $logDate =~ /^(\d\d\d\d)(\d\d)$/ ) {
-		$logYear  = $1;
-		$logMonth = $2;
-	}
-	else {
-		_printMsg( $session, "!Error in date $logDate - must be YYYYMM" );
-		return;
-	}
+    my $logMonth;
+    my $logYear;
+    if ( $logDate =~ /^(\d\d\d\d)(\d\d)$/ ) {
+        $logYear  = $1;
+        $logMonth = $2;
+    }
+    else {
+        _printMsg( $session, "!Error in date $logDate - must be YYYYMM" );
+        return;
+    }
 
-	my $logMonthYear =
-	  $Foswiki::Time::ISOMONTH[ $logMonth - 1 ] . ' ' . $logYear;
+    my $logMonthYear =
+      $Foswiki::Time::ISOMONTH[ $logMonth - 1 ] . ' ' . $logYear;
 
-	# Do a single data collection pass on the temporary copy of logfile,
-	# then process each web once.
-	my @users =
-	  _collectLogData( $session, "1 $logMonthYear", $web, $topic, $script );
+    # Do a single data collection pass on the temporary copy of logfile,
+    # then process each web once.
+    my @users =
+      _collectLogData( $session, "1 $logMonthYear", $web, $topic, $script );
 
-	return join ' ', @users;
+    return join ' ', @users;
 }
 
 sub _collectLogData {
-	my ( $session, $start, $web, $topic, $script ) = @_;
+    my ( $session, $start, $web, $topic, $script ) = @_;
 
-	$start = Foswiki::Time::parseTime($start);
+    $start = Foswiki::Time::parseTime($start);
 
-	my @users = ();
+    my @users = ();
 
-	my $it = $session->logger->eachEventSince( $start, 'info' );
-	while ( $it->hasNext() ) {
-		my $line = $it->next();
-		my ( $date, $logFileUserName, $opName, $webTopic, $notes, $ip ) =
-		  @$line;
+    my $it = $session->logger->eachEventSince( $start, 'info' );
+    while ( $it->hasNext() ) {
+        my $line = $it->next();
+        my ( $date, $logFileUserName, $opName, $webTopic, $notes, $ip ) =
+          @$line;
 
-		# ignore minor changes - not statistically helpful
-		next if ( $notes && $notes =~ /(minor|dontNotify)/ );
+        # ignore minor changes - not statistically helpful
+        next if ( $notes && $notes =~ /(minor|dontNotify)/ );
 
-		# ignore searches for now - idea: make a "top search phrase list"
-		next if ( $opName && $opName =~ /(search)/ );
+        # ignore searches for now - idea: make a "top search phrase list"
+        next if ( $opName && $opName =~ /(search)/ );
 
-		# ignore "renamed web" log lines
-		next if ( $opName && $opName =~ /(renameweb)/ );
+        # ignore "renamed web" log lines
+        next if ( $opName && $opName =~ /(renameweb)/ );
 
-		# ignore "change password" log lines
-		next if ( $opName && $opName =~ /(changepasswd)/ );
+        # ignore "change password" log lines
+        next if ( $opName && $opName =~ /(changepasswd)/ );
 
-		next if ( $script =~ /view|save/ && ( $opName ne $script ) );
+        next if ( $script =~ /view|save/ && ( $opName ne $script ) );
 
-		my ( $webName, $topicName ) =
-		  ( $webTopic =~
+        my ( $webName, $topicName ) =
+          ( $webTopic =~
 /(^$Foswiki::regex{webNameRegex})\.($Foswiki::regex{wikiWordRegex}$|$Foswiki::regex{abbrevRegex}|.+)/
-		  );
+          );
 
-		next if ( $webName ne $web );
+        next if ( $webName ne $web );
 
-		next if ( $topicName ne $topic );
+        next if ( $topicName ne $topic );
 
-		push @users, $logFileUserName;
+        push @users, $logFileUserName;
 
-	}
+    }
 
-	return @users;
+    return @users;
 
 }
